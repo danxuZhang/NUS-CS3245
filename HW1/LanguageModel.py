b275dc344958c80da73f0a0d99b904c3ce4f0056
DanxuZhang <dxzhang49@gmail.com>
2023-01-23 00:24:11 +0800

    Add Getter and Update Accessibility
    The language model's name cannot be accessed outside the model. Now
    add a getter to get the model's name.
    
    Methods tokenize(), add_tokens() are not used outside the model. Now
    changed them to be private methods.
    
    The function of the method add_string() is more acurately described as
    add string to train the model. Now its name is changed to be train()
    
    Sort methods according to accessibility.

--- HW1/LanguageModel.py
+++ HW1/LanguageModel.py
@@ -17,13 +17,51 @@ class LanguageModel:
         """Constructor of the language model and initialize its name.
         Args:
             name: name of the language model (e.g. English, Malay)
-            n: n-gram model, by default it's a 4-gram model
+            n: n-gram model, by default it's a 4-gram character model
         """
         self._name = name
         self._occurs = defaultdict(int)
         self._n = n
 
-    def tokenize(self, text: str) -> list:
+    def get_name(self) -> str:
+        return self._name
+
+    def train(self, text: string) -> None:
+        """Convert text to tokens and then add tokens to the model
+        Args:
+            text: text to be converted and added to the model
+        """
+        tokens = self._tokenize(text)
+        self._add_tokens(tokens)
+
+    def cal_log_prob(self, text: str) -> float:
+        """Calculate probability for a given text in a language model in log 10
+        First perform smoothing, default add 1 smoothing, then normalize the frequency distribution with probabilities.
+        Calculation of probabilities is done in log form, otherwise the number will be too small.
+        Args:
+            text: a string to be calculated
+        Returns:
+            calculated probabilities in log 10
+        """
+        # convert the text to tokens
+        tokens = self._tokenize(text)
+
+        # perform smoothing
+        smoothed_dict = self._smoothing(tokens)
+
+        # normalize dictionary by calculating the probabilities
+        n = sum(smoothed_dict.values())  # total number
+        probabilities = defaultdict(float)
+        for token in smoothed_dict:
+            probabilities[token] += smoothed_dict[token] / n
+
+        # calculate probabilities in log-10
+        prob = 0.0
+        for token in tokens:
+            prob += log10(probabilities[token])
+        return prob
+
+    def _tokenize(self, text: str) -> list:
         """Convert an input string to a list of tokens.
 
         Token is a string of n characters including spaces, by default it's 4 characters(4-gram)
@@ -45,48 +83,13 @@ class LanguageModel:
             tokens.append(sub_str)
         return tokens
 
-    def add_string(self, text: string) -> None:
-        """Convert text to tokens and then add tokens to the model
-        Args:
-            text: text to be converted and added to the model
-        """
-        tokens = self.tokenize(text)
-        self.add_tokens(tokens)
-
-    def add_tokens(self, tokens: list) -> None:
+    def _add_tokens(self, tokens: list) -> None:
         """Add tokens to the dictionary
         Args:
             tokens: a list of tokens to be added.
         """
         for token in tokens:
             self._occurs[token] += 1
-
-    def cal_log_prob(self, text: str) -> float:
-        """Calculate probability for a given text in a language model in log 10
-        First perform smoothing, default add 1 smoothing, then normalize the frequency distribution with probabilities.
-        Calculation of probabilities is done in log form, otherwise the number will be too small.
-        Args:
-            text: a string to be calculated
-        Returns:
-            calculated probabilities in log 10
-        """
-        # convert the text to tokens
-        tokens = self.tokenize(text)
-
-        # perform smoothing
-        smoothed_dict = self._smoothing(tokens)
-
-        # normalize dictionary by calculating the probabilities
-        n = sum(smoothed_dict.values())  # total number
-        probabilities = defaultdict(float)
-        for token in smoothed_dict:
-            probabilities[token] += smoothed_dict[token] / n
-
-        # calculate probabilities in log-10
-        prob = 0.0
-        for token in tokens:
-            prob += log10(probabilities[token])
-        return prob
 
     def _smoothing(self, tokens: list = None, n: int = 1) -> defaultdict:
         """Smoothing, default add one smoothing"""
@@ -98,9 +101,9 @@ class LanguageModel:
 
 def test():
     lm1 = LanguageModel("Test1")
-    lm1.add_string("Hello world! 123 hello yes Yes yes.")
+    lm1.train("Hello world! 123 hello yes Yes yes.")
     lm2 = LanguageModel("Test2")
-    lm2.add_string("Bye bye! No no no.")
+    lm2.train("Bye bye! No no no.")
     print(lm1._occurs)
     print(lm1.cal_log_prob("hello world yes yes"))
     print(lm2.cal_log_prob("hello world yes yes"))
