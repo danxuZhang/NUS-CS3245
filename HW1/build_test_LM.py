0ccbb1c416b375e22d489dc80bbe4706efc4c388
DanxuZhang <dxzhang49@gmail.com>
2023-01-24 15:41:20 +0800

    Add building and testing functions
    Build and train language models from input files, then compare the
    result against correct result.

--- HW1/build_test_LM.py
+++ HW1/build_test_LM.py
@@ -5,10 +5,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import re
-import nltk
+import os
 import sys
 import getopt
+
+from LanguageModel import LanguageModel
 
 
 def build_LM(in_file):
@@ -19,6 +20,19 @@ def build_LM(in_file):
     print("building language models...")
     # This is an empty method
     # Pls implement your code below
+
+    lms = {}
+
+    with open(in_file) as f:
+        for line in f:
+            # exact label and actual content from the input line
+            (lang, text) = line.strip("\r\n").split(" ", 1)
+            if lang not in lms.keys():
+                lms[lang] = LanguageModel(lang)
+            lms[lang].train(text)
+
+    print("langauge model built!")
+    return lms
 
 
 def test_LM(in_file, out_file, LM):
@@ -31,6 +45,19 @@ def test_LM(in_file, out_file, LM):
     # This is an empty method
     # Pls implement your code below
 
+    with open(in_file) as in_f, open(out_file, "w") as out_f:
+        for line in in_f:
+            # try each langauge model and find the one with the highest probability
+            probs = {}
+            for lm in LM.values():
+                probs[lm.get_name()] = lm.cal_log_prob(line)
+            max_lm = max(probs, key=probs.get)
+            # write result to the output file
+            out_f.write(max_lm + " " + line)
+
+    print("langauge models test finished!")
+    # run evaluation module
+    os.system("python3 eval.py input.predict.txt input.correct.txt")
 
 def usage():
     print(
