0a5cb1986bd01aebd6a193cef4257c7d5656224e
DanxuZhang <dxzhang49@gmail.com>
2023-02-03 20:57:28 +0800

    Add skeleton files for homework 2.
    These files are downloaded from CS3245 course website.

---
+++ HW2/index.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import re
+import nltk
+import sys
+import getopt
+
+def usage():
+    print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+
+def build_index(in_dir, out_dict, out_postings):
+    """
+    build index from documents stored in the input directory,
+    then output the dictionary file and postings file
+    """
+    print('indexing...')
+    # This is an empty method
+    # Pls implement your code in below
+
+input_directory = output_file_dictionary = output_file_postings = None
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o == '-i': # input directory
+        input_directory = a
+    elif o == '-d': # dictionary file
+        output_file_dictionary = a
+    elif o == '-p': # postings file
+        output_file_postings = a
+    else:
+        assert False, "unhandled option"
+
+if input_directory == None or output_file_postings == None or output_file_dictionary == None:
+    usage()
+    sys.exit(2)
+
+build_index(input_directory, output_file_dictionary, output_file_postings)
